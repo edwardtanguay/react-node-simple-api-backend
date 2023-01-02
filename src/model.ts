cccccf48e64e1256c535ca7cc977fadf503b63b5
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-02 23:15:46 +0100

    finished

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,8 @@
-export const getFlashcards = () => {
-	return ['from model.ts'];
+export const getTest = () => {
+	const datetime = new Date();
+	return {
+		message: `API test accessed at ${datetime.toISOString()}`
+	}
 }
 
 export const getApiInstructions = () => {
@@ -8,13 +11,19 @@ export const getApiInstructions = () => {
 	body {
 		background-color: #444;
 		padding: 1rem;
-		color: #fff;
-		font-family: courier;
+		color: #ccc;
+		font-family: sans-serif;
 	}
 	code {
 		background-color: #333;
 	}
+	a {
+		color: orange;
+	}
 </style>
 <h1>SQLite Site API</h1>
+<ul>
+	<li><a href="/test">/test</a> - shows date/time as test</li>
+</ul>
 	`;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
 import express from 'express';
 import cors from 'cors';
-import * as config from './config.js';
 import * as model from './model.js';
 
 const app = express();
 app.use(cors());
+const port = 3610;
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send(model.getApiInstructions());
 });
 
 app.get('/test', (req: express.Request, res: express.Response) => {
-	const datetime = new Date();
-	res.json({
-		message: `API test accessed at ${datetime.toISOString()}`
-	});
+	res.json(model.getTest());
 });
 
-app.get('/flashcards', (req: express.Request, res: express.Response) => {
-	res.json(model.getFlashcards());
-});
-
-app.listen(config.port, () => {
-	console.log(`listening on port http://localhost:${config.port}`);
+app.listen(port, () => {
+	console.log(`listening on port http://localhost:${port}`);
 });
